7722cc83a04a684e64dca5d6e27d42b2cf4fbaaf
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-04-23 16:15:35 +0300

    link back chain setting (#88)
    * link back chain setting
    
    * update to latest strapi

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -566,6 +566,10 @@ export interface ApiChainSettingChainSetting
       'manyToOne',
       'api::lock-token-whitelist.lock-token-whitelist'
     >;
+    projects_informations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::projects-information.projects-information'
+    >;
     publishedAt: Schema.Attribute.DateTime;
     scannerTxUrl: Schema.Attribute.String;
     Show: Schema.Attribute.Boolean;
@@ -1491,7 +1495,7 @@ export interface ApiProjectsInformationProjectsInformation
     Block: Schema.Attribute.Media<'images'>;
     blockLocations: Schema.Attribute.String;
     chain_setting: Schema.Attribute.Relation<
-      'oneToOne',
+      'manyToOne',
       'api::chain-setting.chain-setting'
     >;
     coingeckoKey: Schema.Attribute.String;
